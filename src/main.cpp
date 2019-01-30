1cad0075e8f8e523d710bdba2edcb62428f12822
Deand <deandreyanov@gmail.com>
2019-01-30 14:30:48 +0300

    create repo

---
+++ src/main.cpp
@@ -0,0 +1,73 @@
+/*********
+  Руи Сантос (Rui Santos)
+  Более подробно о проекте на: http://randomnerdtutorials.com
+  Пример в IDE Arduino: File > Examples > Arduino OTA > BasicOTA.ino
+                       (Файл > Примеры > Arduino OTA > BasicOTA.ino)
+*********/
+
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+// замените значения в этих константах на те,
+// что соответствуют вашей сети:
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");  //  "Загрузка"
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    //  "Соединиться не удалось! Перезагрузка..."
+    delay(5000);
+    ESP.restart();
+  }
+
+  // строчка для номера порта по умолчанию
+  // можно вписать «8266»:
+  // ArduinoOTA.setPort(8266);
+
+  // строчка для названия хоста по умолчанию;
+  // можно вписать «esp8266-[ID чипа]»:
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // строчка для аутентификации
+  // (по умолчанию никакой аутентификации не будет):
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");  //  "Начало OTA-апдейта"
+
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");  //  "Завершение OTA-апдейта"
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    //  "Ошибка при аутентификации"
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    //  "Ошибка при начале OTA-апдейта"
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    //  "Ошибка при подключении"
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    //  "Ошибка при получении данных"
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    //  "Ошибка при завершении OTA-апдейта"
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");  //  "Готово"
+  Serial.print("IP address: ");  //  "IP-адрес: "
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  ArduinoOTA.handle();
+}
