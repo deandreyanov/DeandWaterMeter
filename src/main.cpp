b856fa95ac066178e8ae9fc90a932da6f48a2dc1
Deand <deandreyanov@gmail.com>
2019-04-13 00:08:22 +0300

    add save and load settings to EEPROM

--- src/main.cpp
+++ src/main.cpp
@@ -30,11 +30,7 @@
 #define HOT_INPUT 5 //GPIO 5 = D1
 #define COLD_INPUT 4 //GPIO 4 = D2
 //#define SETT_INPUT 4 //GPIO 4 = D2
-/*
-#define ESP_CONNECT_TIMEOUT 15000UL // Время подключения к точке доступа, ms
-#define SERVER_TIMEOUT 12000UL // Время ответа сервера, ms
-#define LITRES_PER_IMPULS_DEFAULT 10  // 10 литров на импульс
-*/
+
 // предыдущее состояние импульсного выхода
 int prev_cold;
 int prev_hot;
@@ -44,8 +40,8 @@ int curr_cold;
 int curr_hot;
 
 // сохранённые показания счётчиков
-float save_cold;
-float save_hot;
+ float save_cold;
+ float save_hot;
 
 const char* host = "esp8266-webupdate";
 ESP8266WebServer httpServer(80);
@@ -53,7 +49,9 @@ ESP8266HTTPUpdateServer httpUpdater;
 
 WidgetLCD lcd(V1);
 
-float read_state(int PinNumber) {
+strSettings mySett;
+
+int read_state(int PinNumber) {
   float state;
   pinMode(PinNumber, OUTPUT);
   digitalWrite(PinNumber, HIGH);
@@ -63,6 +61,12 @@ float read_state(int PinNumber) {
 }
 
 void setup() {
+  if (loadSettings(mySett)) {
+
+  } else {
+
+  }
+
   save_cold = 0;
   save_hot = 0;
 
@@ -86,46 +90,6 @@ void setup() {
 
   Blynk.config(auth);
 
-  // строчка для номера порта по умолчанию
-  // можно вписать «8266»:
-  // ArduinoOTA.setPort(8266);
-
-  // строчка для названия хоста по умолчанию;
-  // можно вписать «esp8266-[ID чипа]»:
-  // ArduinoOTA.setHostname("myesp8266");
-
-  // строчка для аутентификации
-  // (по умолчанию никакой аутентификации не будет):
-  // ArduinoOTA.setPassword((const char *)"123");
-
-  /*
-  ArduinoOTA.onStart([]() {
-    Serial.println("Start");  //  "Начало OTA-апдейта"
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");  //  "Завершение OTA-апдейта"
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    //  "Ошибка при аутентификации"
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    //  "Ошибка при начале OTA-апдейта"
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    //  "Ошибка при подключении"
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    //  "Ошибка при получении данных"
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    //  "Ошибка при завершении OTA-апдейта"
-  });
-  ArduinoOTA.begin();
-  Serial.println("Ready");  //  "Готово"
-  Serial.print("IP address: ");  //  "IP-адрес: "
-  Serial.println(WiFi.localIP());
-  */
 
   MDNS.begin(host);
 
@@ -141,58 +105,43 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   pinMode(LED, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
-//  pinMode(COLD_INPUT, INPUT);
-//  pinMode(HOT_INPUT, INPUT);
-//  pinMode(SETT_INPUT, INPUT);
-  Serial.println("Setup");
-  Serial.println(read_state(COLD_INPUT));
-  Serial.println(read_state(HOT_INPUT));
 }
-void send_2_blynk()
-{
+// Отправка показаний на Blynk
+void send_2_blynk() {
   Blynk.run();
-  if (Blynk.connect(SERVER_TIMEOUT)){
-    //Serial.println("Blynk.connect");
+  if (Blynk.connect(SERVER_TIMEOUT)) {
+    lcd.clear();
+    lcd.print(0, 0, "C:"); // use: (position X: 0-15, position Y: 0-1, "Message you want to print")
+    lcd.print(0, 1, "H:");
 
-    lcd.clear(); //Use it to clear the LCD Widget
-    lcd.print(0, 0, save_cold); // use: (position X: 0-15, position Y: 0-1, "Message you want to print")
-    lcd.print(0, 1, save_hot);
-    //Serial.println(save_cold);
-    //Serial.println(save_hot);
-
+    lcd.print(2, 0, mySett.save_cold); // use: (position X: 0-15, position Y: 0-1, "Message you want to print")
+    lcd.print(2, 1, mySett.save_hot);
   } else {
       Serial.println("Blynk.connect error");
   }
 }
 
 void loop() {
-  //Blynk.run();
-  //ArduinoOTA.handle();
   httpServer.handleClient();
   MDNS.update();
 
   curr_cold = read_state(COLD_INPUT);
   curr_hot = read_state(HOT_INPUT);
 
-  /*Serial.println("------------------------");
-  Serial.println(curr_cold);
-  Serial.println(curr_hot);
-*/
-  //Serial.println(read_state(SETT_INPUT));
-
   if (prev_cold != curr_cold) {
     if (curr_cold == 0) {
-      save_cold = save_cold + LITRES_PER_IMPULS_DEFAULT; // / 1000;
+      mySett.save_cold += LITRES_PER_IMPULS_DEFAULT;
     }
     prev_cold = curr_cold;
   }
 
   if (prev_hot != curr_hot) {
     if (curr_hot == 0) {
-      save_hot = save_hot + LITRES_PER_IMPULS_DEFAULT;// / 1000;
+      mySett.save_hot += LITRES_PER_IMPULS_DEFAULT;
     }
     prev_hot = curr_hot;
   }
+  saveSettings(mySett);
   send_2_blynk();
 
   //ESP.deepSleep(5e06);
--- src/settings.h
+++ src/settings.h
@@ -1,6 +1,5 @@
 #include <Logging.h>
 
-#define FIRMWARE_VERSION "0.7"
 /*
     Уровень логирования
 */
@@ -8,72 +7,28 @@
 
 #define ESP_CONNECT_TIMEOUT 15000UL // Время подключения к точке доступа, ms
 #define SERVER_TIMEOUT 12000UL // Время ответа сервера, ms
-#define LITRES_PER_IMPULS_DEFAULT 10  // 10 литров на импульс
-
-#define BLYNK_KEY_LEN 34
-
-#define FAKE_CRC 0422
-
-#define CURRENT_VERSION 1
+#define LITRES_PER_IMPULS_DEFAULT 0.001  // 10 литров на импульс
+#define MY_CRC 2517 // если контрольная суммма сошлась - значит настройки сохранялись
 
 /*
 Настройки хранящиеся EEPROM
 */
-struct Settings
-{
-    uint8_t  version;      //Версия конфигурации
-
-    uint8_t  reserved;
-
-    //SEND_BLYNK
-
-    //уникальный ключ устройства blynk
-    char     blynk_key[BLYNK_KEY_LEN];
-
+struct strSettings {
     /*
     Показания счетчиках в кубометрах,
     введенные пользователем при настройке
     */
-    float    channel0_start;
-    float    channel1_start;
-
-    /*
-    Кол-во литров на 1 импульс
-    */
-    uint16_t liters_per_impuls;
-
-    /*
-    Кол-во импульсов Attiny85 соответствующие показаниям счетчиков,
-    введенных пользователем при настройке
-    */
-    uint32_t impulses0_start;
-    uint32_t impulses1_start;
-
-    /*
-    Не понятно, как получить от Blynk прирост показаний,
-    поэтому сохраним их в памяти каждое включение
-    */
-    uint32_t impulses0_previous;
-    uint32_t impulses1_previous;
-
-    /*
-    Зарезервируем кучу места, чтобы не писать конвертер конфигураций.
-    Будет актуально для On-the-Air обновлений
-    */
-    uint8_t  reserved2[256];
-
-    /*
-    Контрольная сумма, чтобы гарантировать корректность чтения настроек
-    */
-    uint16_t crc;
+    float save_cold;
+    float save_hot;
+    int myCRC;
 };
 
 /*
 Сохраняем конфигурацию в EEPROM
 */
-void saveConfig(const Settings &sett);
+void saveSettings(strSettings &sett);
 
 /*
 Читаем конфигурацию из EEPROM
 */
-bool loadConfig(Settings &sett);
+bool loadSettings(strSettings &sett);
